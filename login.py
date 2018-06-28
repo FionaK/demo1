feb2a9a11e20a29c14ee5aeb5f810cf08348d31b
Fiona K Owuor <fiona@localhost.localdomain>
2018-06-28 11:02:34 +0300

    new update

--- login.py
+++ login.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
-class Signin(object):
-	def __init__(self, details):
-  		self.details={}
-  	def register(self):
-  		firstname=raw_input('Enter your firstname')
-  		lastname=raw_input('Enter your lastname')
-  		username=raw_input('Enter your username')
-  		password=raw_input('Enter your password')
-  		email=raw_input('Enter your email')
-  		self.details.update({username:password})
-  		def login(self):
-  			username=raw_input('Enter your username')
-  			password=raw_input('Enter your password')
-  			if password==self.details[username]:
-  				print ('ACCESS GRANTED')
-  			else:					
-  				print('INVALID DETAILS')
+ class Signin(object):
+	def __init__(self):
+		self.details={}
+	def register(self):
+		firstname=input('Enter your firstname')
+		lastname=input('Enter your lastname')
+		username=input('Enter your username')
+		password=input('Enter your password')
+		email=input('Enter your email')
+		self.details.update({username:password})
+		print ('Resgistration successful')
+	def login(self):
+		username=input('Enter your username')
+		password=input('Enter your password')
+		if username in self.details:
+			if password==self.details[username]:
+				print ('ACCESS GRANTED')
+			else:
+				print('INVALID DETAILS')
+		else:
+			print ("Please register first")
   			
