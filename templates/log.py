1f45edd17764d86325e1f05083b98f16eae30bab
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-01 18:53:00 +0300

    update

--- templates/log.py
+++ templates/log.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('login.html')
+@app.route('/register')
 def register():
 	return render_template('register.html')
+@app.route('/comments')
+def comments():
+        return render_template('comments.html')
 			
 	
 	 
