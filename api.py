00e7ddb4662b408dcab36e5e31a085e2a5fcc964
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-03 10:49:52 +0300

    update password

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ import hashlib
 import json
 
 access_token_path = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-consumer_key="GrMDfVCKLTq6POB6gi0VBGqm6doxrFCn"
-consumer_secret="PUSL6g38YYaXhXGv"
-response = requests.get(access_token_path, auth=(consumer_key, consumer_secret)).text
+#consumer_key="GrMDfVCKLTq6POB6gi0VBGqm6doxrFCn"
+#consumer_secret="PUSL6g38YYaXhXGv"
+response = requests.get(access_token_path, auth=("GrMDfVCKLTq6POB6gi0VBGqm6doxrFCn", "PUSL6g38YYaXhXGv")).text
 res=json.loads(response)
 access_token=res ['access_token']
 
@@ -15,24 +15,31 @@ access_token=res ['access_token']
 url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 headers = { "Authorization": "Bearer %s" % access_token }
 
+BusinessShortCode=174379
+passkey='bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+Timestamp=20180702153955
+ps_wd = ('BusinessShortCode' + 'passkey' + 'Timestamp')
+password = base64.b64encode(ps_wd.encode('utf=8'))
+
+
 
 payload = {
   "BusinessShortCode": "174379",
-  "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTgwNzAyMTE1NDU1 ",
+  "Password": password,
   "Timestamp": "20180702153955",
   "TransactionType": "CustomerPayBillOnline",
   "Amount": "2",
   "PartyA": "254702838079",
   "PartyB": "174379",
   "PhoneNumber": "254702838079",
-  "CallBackURL": "http://mpesa-requestbin.herokuapp.com/pufk94pu",
+  "CallBackURL": "http://mpesa-requestbin.herokuapp.com/1der4d01",
   "AccountReference": "test",
   "TransactionDesc": "test"
   }
 
 def get_access_token():
   url = self.api + self.access_token_path
-  response = self.payload.get(url, auth=(self.consumer_key, self.consumer_secret))
+  response = payload.get(url, auth=("GrMDfVCKLTq6POB6gi0VBGqm6doxrFCn", "PUSL6g38YYaXhXGv"))
   if response.status_code == 200:
     data = response.json()
     self.access_token = data['access_token']
