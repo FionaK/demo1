5dafa40983fa7ee186bcd107fbc5d4c58ffc6ba8
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-06 09:36:03 +0300

    access token

---
+++ new_rest.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+import datetime
+from functools import wraps
+import jwt
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='fifi'
+details = {}
+posts=[]
+
+user1= 'fk'
+pwd= 'fko'
+
+def token_required(k):
+    @wraps(k)
+    def decorated(*args, **kwargs):
+    	token = request.args.get('token')
+    	if not token:
+    		return jsonify({'message' : 'Token is missing'})
+    	try:
+    		data = jwt.decode(token, app.config['SECRET_KEY'])
+    	except:
+    		return jsonify({'message' : 'Token is invalid'})
+    	return k(*args, **kwargs)
+    return decorated
+    
+
+@app.route('/', methods= ['POST','GET'])
+def test():
+	return jsonify({'message': 'welcome!'})
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	firstname=request.get_json()['firstname']
+	lastname=request.get_json()['lastname']
+	username=request.get_json()['username']
+	password=request.get_json()['password']
+	email=request.get_json()['email']
+	details.update({username:{"first_name":firstname,"last_name":lastname,"email":email,"pass_wd":password}})
+	return jsonify({'message':'successful registration'})
+
+@app.route('/login', methods= ['GET', 'POST'])
+def login():
+	username=request.get_json()['username']
+	password=request.get_json()['password']
+	auth= request.authorization
+	
+		
+
+	if username in details:
+		if password==details[username]["password"]:
+			token= jwt.encode ({'user':auth.username, 'exp':datetime.datetime.utcnow + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'])
+			return jsonify({"message": "Login successful", 'access-token' : token.decode('UTF-8')})
+		else:
+			return jsonify({'message': "invalid password or username"})
+
+@app.route('/comments', methods=['GET', 'POST'])
+def comments():
+	comment=request.get_json()["comment"]
+	posts.append(comment)
+	return jsonify({"message": "comment uploaded"})
+
+	
+@app.route('/display', methods=['GET'])
+def display_comments():
+	output={}
+	for each in posts:
+		output.update({posts.index(each):each})
+		return jsonify(output)
+
+@app.route('/delete_comment/<int:commentid>', methods=['DELETE'])	
+def delete_comment(commentid):
+	del posts[commentid]
+	return jsonify ({"message": "your comment has been deleted"})
+
+
+if __name__ == '__main__':
+	app.run( port=5050)
